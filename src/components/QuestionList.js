dc403aff4b5c805f4a85c3f6846e5a8f5a7592c1
lizdmukami <lizdmukami@gmail.com>
2023-01-19 06:56:36 +0300

    display a list of all questions when page is rendered

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,24 @@
-import React from "react";
+import QuestionItem from "./QuestionItem";
+import React, { useEffect, useState } from "react";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(res => res.json())
+    .then(questions => setQuestions(questions))
+  }, []);
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map((question) => (
+        <QuestionItem
+          key={question.id}
+          question={question}
+        />
+      ))}</ul>
     </section>
   );
 }
